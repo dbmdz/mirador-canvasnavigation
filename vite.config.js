cabd8a365bd06d198e3783a2f9880daa0a4c7370
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2026-06-03 13:01:34 +0200

    Remove obsolete dependency

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 
 const buildMode = process.env.BUILD_MODE ?? "plugin";
 
@@ -28,7 +27,7 @@ const pluginConfig = {
       },
     },
   },
-  plugins: [react(), dts({ include: ["src"], exclude: ["src/demo.js"]})],
+  plugins: [react()],
   server: {
     open: true,
   },
